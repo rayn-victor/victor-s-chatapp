9b52c4c889bdcd7e10b30b85c866da6912e03efa
Rayn Victor <rayn@Rayns-Air.fc.ca>
2020-01-29 08:42:14 -0500

    building server

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+// socket goes here
+const io = require('socket.io')();
+
 const port = process.env.PORT || 3030;
 
 // tell express where our static files are (js, images, css etc)
@@ -10,6 +13,19 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+//normally its just app.listen but socket needs a server
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
+});
+
+// attach our chat server to our app
+io.attach(server); //basically a mailbox, bunch of ports
+
+io.on('connection', function(socket) { // socket is your connection
+    console.log('A user has connected...');
+    socket.emit('connected', { sID: socket.id, message: "new connection" });
+
+    socket.on('disconnect', function() {
+        console.log('A user has disconnected.');
+    })
 });
--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,1 +1,21 @@
 // imports always go first - if we're importing anything
+
+const socket = io();
+
+
+//the packet is whatever data we sebd through with the connect event
+//from the server
+function setUserID(packet) {
+    // debugger;
+    console.log(packet);
+}
+
+//message for 'disconnect'
+function showDisconnectMessage() {
+    console.log('A user has left.');
+}
+
+
+
+socket.addEventListener('connected', setUserID); //catchhing an id for the user when they connect
+socket.addEventListener('disconnect', showDisconnectMessage);
